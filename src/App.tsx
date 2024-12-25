8923ebd67967ac4e0b6cb9ad4bbfcfa90833498a
Thuan Nguyen <thuannguyenhuu11@gmail.com>
2024-12-25 23:37:04 +0700

    Update sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ import LoginPage from './pages/LoginPage';
 import SettingsPage from './pages/SettingsPage';
 import ProfilePage from './pages/ProfilePage';
 import { useAuthStore } from './store/useAuthStore';
+import { useThemeStore } from './store/useThemeStore';
 import { useEffect } from 'react';
 import { Loader } from 'lucide-react';
 import { Toaster } from 'react-hot-toast';
 
 const App = () => {
     const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+    const { theme } = useThemeStore();
 
     useEffect(() => {
         checkAuth();
@@ -27,7 +29,7 @@ const App = () => {
         );
 
     return (
-        <div data-theme="retro">
+        <div data-theme={theme}>
             <Navbar />
             <Routes>
                 <Route path="/" element={authUser ? <Homepage /> : <Navigate to="/login" />} />
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,5 +1,24 @@
+import { useChatStore } from '../store/useChatStore';
+import Sidebar from '../components/Sidebar';
+import NoChatSelected from '../components/NoChatSelected';
+import ChatContainer from '../components/ChatContainer';
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+    const { selectedUser } = useChatStore();
+
+    return (
+        <div className="h-screen bg-base-200">
+            <div className="flex items-center justify-center px-4 pt-20">
+                <div className="bg-base-100 rounded-lg shadow-cl w-full max-w-6xl h-[calc(100vh-8rem)]">
+                    <div className="flex h-full overflow-hidden rounded-lg">
+                        <Sidebar />
+
+                        {!selectedUser ? <NoChatSelected /> : <ChatContainer />}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Homepage;
